5105c6f2c766b4a7a49dea12bcc1a9432ab3f33c
Phosphorusss <phosphorus959@gmail.com>
2023-10-27 12:01:53 +0300

    add Cli Makefile

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+    public static String userName;
+    public static void getUserName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+    }
+}
