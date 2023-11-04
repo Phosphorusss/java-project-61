6e9d9ce11c6821e8abfdd49a575260cd141a0696
Phosphorusss <phosphorus959@gmail.com>
2023-11-04 12:48:07 +0300

    game prime fix 2

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -11,7 +11,7 @@ public class Prime {
         System.out.println("Question: " + number);
         String correctAnswer = "";
 
-        if (number == 1) {
+        if (number == 1 || number == 0) {
             correctAnswer = "no";
         }
         for (var i = 2; i < number; i++) {
